fb2f0c1d46f65abf4c28a2a4c422f557ebe13e99
afurkank <furkan.karacikq@gmail.com>
2023-12-05 16:07:41 +0300

    Add dendrogram and silhouette graph plotting code for part3.py

--- Ceng499/Assignment 2 Data and Source Codes/Part3/part3_dimensionalityreduction.py
+++ Ceng499/Assignment 2 Data and Source Codes/Part3/part3_dimensionalityreduction.py
@@ -1,4 +1,20 @@
 import pickle
-
+import numpy as np
+from sklearn.cluster import AgglomerativeClustering
 
 dataset = pickle.load(open("../data/part3_dataset.data", "rb"))
+
+K_values = [2, 3, 4, 5]
+
+linkages = ['single', 'complete']
+
+metrics = ['euclidean', 'cosine']
+
+for K in K_values:
+    for linkage in linkages:
+        for metric in metrics:
+            clustering = AgglomerativeClustering(
+                n_clusters=2,
+                metric='cosine',
+                linkage='single',
+            ).fit_predict(dataset)
