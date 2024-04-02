6f76c6cdbdbb09c9d8ded7a28027b0f5a316dd70
e2310209 <e2310209@ceng.metu.edu.tr>
2024-04-02 21:21:30 +0300

    add ceng443 directory and lab1 materials

---
+++ ceng443/sample codes/5/Main.java
@@ -0,0 +1,40 @@
+// upcasting & downcasting 
+
+abstract class SweetProducer { 
+    public abstract void produceSweet();  
+} 
+
+class CakeProducer extends SweetProducer { 
+    @Override 
+    public void produceSweet() { 
+        System.out.println("Cake produced"); 
+    } 
+} 
+class ChocolateProducer extends SweetProducer { 
+    @Override 
+    public void produceSweet() { 
+        System.out.println("Chocolate produced"); 
+    } 
+    // spesific method 
+    public void moreCacao() {
+        System.out.println("More cacao added"); 
+    } 
+} 
+class CookieProducer extends SweetProducer { 
+    @Override  
+    public void produceSweet() { 
+        System.out.println("Cookie produced");  
+    }
+}
+
+public class Main {
+    public static void main(String[] args) { 
+        SweetProducer s1 = new ChocolateProducer();
+        SweetProducer s2 = (SweetProducer) new CakeProducer(); // No need for "explicit" upcasting 
+        s1.produceSweet(); 
+        s2.produceSweet(); 
+        // s1.moreCacao(); Error: cannot find moreCacao()  
+        ((ChocolateProducer) s1).moreCacao();
+        // ((ChocolateProducer) s2).moreCacao(); Exception: CakeProducer cannot be cast to ChocolateProducer
+    }
+}
